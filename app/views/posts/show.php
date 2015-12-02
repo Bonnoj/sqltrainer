648b2ec215c75e03d12bb53c5a5747694f93372f
bonnoj <jans0130@hz.nl>
2015-12-02 12:39:12 +0100

    Deleted old files

--- app/views/posts/show.php
+++
@@ -1,9 +0,0 @@
-<a href="{{ urlFor('home') }}">Back home</a>
-
-<h1>{{ post.title }}</h1>
-
-<p>{{ post.body }}</p>
-
-<div class="author">
-	By {{ post.first_name}} {{ post.last_name }} on {{ post.created }}
-</div>
